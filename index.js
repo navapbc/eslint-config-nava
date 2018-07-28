c9ee6def9802de942c181f30f15533d308dbb286
Kenshiro Nakagawa <kencheeto@gmail.com>
2018-07-28 14:55:54 -0700

    remove style-only rules, add additional correctness rules, upgrade deps

--- index.js
+++ index.js
@@ -2,19 +2,15 @@ module.exports = {
   extends: ['standard', 'standard-react'],
   rules: {
     /*
-      Standard overrides
-    */
-    semi: ['error', 'always'], // churn-expensive to enforce
-    'space-before-function-paren': ['error', 'never'],
-
-    /*
       Nava-specific rules
     */
-    'linebreak-style': ['error', 'unix'],
-    'sort-vars': ['error', {ignoreCase: true}],
     'no-alert': 'error',
     'no-var': 'error',
     'prefer-const': 'error',
-    'prefer-arrow-callback': 'error'
+    'prefer-arrow-callback': 'error',
+    'promise/no-native': 'error',
+    'promise/catch-or-return': 'error',
+    'promise/no-return-wrap': 'error',
+    'promise/valid-params': 'error'
   }
 };
