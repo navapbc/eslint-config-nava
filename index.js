176315ac5abc9324d480b9fcb8549ff5de7df638
Sawyer Hollenshead <git@sawyerh.com>
2019-07-27 13:39:22 -0400

    Revert 'prettier' formatting

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = {
-  extends: ["standard", "standard-react"],
+  extends: ['standard', 'standard-react'],
   rules: {
     /*
       Nava-specific rules
     */
-    "no-alert": "error",
-    "no-var": "error",
-    "prefer-const": "error",
-    "prefer-arrow-callback": "error",
-    "promise/no-native": "error",
-    "promise/catch-or-return": "error",
-    "promise/no-return-wrap": "error",
-    "promise/valid-params": "error"
+    'no-alert': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-arrow-callback': 'error',
+    'promise/no-native': 'error',
+    'promise/catch-or-return': 'error',
+    'promise/no-return-wrap': 'error',
+    'promise/valid-params': 'error'
   }
-};
+}
